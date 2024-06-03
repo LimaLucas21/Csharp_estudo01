64b5b9007c14c946bb31edb1b6624fb125a4d45c
lucaslima96 <lsousa@tasken.com>
2024-06-03 15:37:45 -0300

    upando projeto

---
+++ Json.cs
@@ -0,0 +1,13 @@
+public class Api
+{
+    public string cep {get; set;} = "";
+    public string logradouro { get; set; } = "";
+    public string complemento { get; set; } = "";
+    public string bairro { get; set; } = "";
+    public string localidade { get; set; } = "";
+    public string uf { get; set; } = "";
+    public string ibge { get; set; } = "";
+    public string qia { get; set; } = "";
+    public string ddd { get; set; } = "";
+    public string siafi { get; set; } = "";
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Digite o Cep desejado: ");
+        string CEP = Console.ReadLine();
+
+        var client = new HttpClient();
+        var url = "https://viacep.com.br/ws/"+ CEP + "/json/";
+
+        var resposta = await client.GetAsync(url);
+
+        if(resposta.IsSuccessStatusCode)
+        {
+            var obj_resp = await resposta.Content.ReadAsStringAsync();
+
+            var objApi = JsonSerializer.Deserialize<Api>(obj_resp);
+
+            Console.WriteLine(
+                "Cep: " + objApi.cep + "\n" +
+                "Logradouro: " + objApi.logradouro);
+        }
+        else
+        {
+            Console.WriteLine("A solicitação falhou devido: " + resposta.StatusCode);
+            Console.WriteLine(await resposta.Content.ReadAsStringAsync());
+        }
+    }
+}
