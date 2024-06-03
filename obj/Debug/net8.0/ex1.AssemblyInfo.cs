f1af27450b007652ac1abf4f481100d3306d3d8a
lucaslima96 <lsousa@tasken.com>
2024-06-03 16:10:38 -0300

    Primeiro commit

--- obj/Debug/net8.0/ex1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ex1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b5b9007c14c946bb31edb1b6624fb125a4d45c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
